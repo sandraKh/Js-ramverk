30df54db24ea48553401780db5dd9c488d50c3d7
Sandra Kullar <41023080+sandraKh@users.noreply.github.com>
2021-08-30 08:44:36 +0200

    First commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,120 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import { render } from "react-dom";
+import ReactQuill, { Quill } from "react-quill";
+import "react-quill/dist/quill.snow.css";
+import "./style.css";
+import * as Icon from 'react-feather'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+// Save button
+const SaveBtn = () => <Icon.Save/>
+
+function save() {
+  var text = this.quill.getText();
+  console.log("text", text)
+}
+
+/*
+ * Custom toolbar component including the custom heart button and dropdowns
+ */
+const CustomToolbar = () => (
+  <div id="toolbar">
+    {/* <select className="ql-font">
+      <option value="arial" selected>
+        Arial
+      </option>
+      <option value="comic-sans">Comic Sans</option>
+      <option value="courier-new">Courier New</option>
+      <option value="georgia">Georgia</option>
+      <option value="helvetica">Helvetica</option>
+      <option value="lucida">Lucida</option>
+    </select> */}
+    <select className="ql-size">
+      <option value="extra-small">Size 1</option>
+      <option value="small">Size 2</option>
+      <option value="medium" selected>
+        Size 3
+      </option>
+      <option value="large">Size 4</option>
+    </select>
+    <select className="ql-color" />
+    <button className="ql-clean" />
+    <button className="ql-save">
+      <SaveBtn />
+    </button>
+  </div>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// Add sizes to whitelist and register them
+const Size = Quill.import("formats/size");
+Size.whitelist = ["extra-small", "small", "medium", "large"];
+Quill.register(Size, true);
+
+// // Add fonts to whitelist and register them
+// const Font = Quill.import("formats/font");
+// Font.whitelist = [
+//   "arial",
+//   "comic-sans",
+//   "courier-new",
+//   "georgia",
+//   "helvetica",
+//   "lucida"
+// ];
+// Quill.register(Font, true);
+
+class Editor extends React.Component {
+  state = { editorHtml: "" };
+
+  handleChange = html => {
+    this.setState({ editorHtml: html });
+  };
+
+  static modules = {
+    toolbar: {
+      container: "#toolbar",
+      handlers: {
+        save: save
+      }
+    }
+  };
+
+  static formats = [
+    "header",
+    "font",
+    "size",
+    "bold",
+    "italic",
+    "underline",
+    "strike",
+    "blockquote",
+    "list",
+    "bullet",
+    "indent",
+    "link",
+    "image",
+    "color"
+  ];
+
+  render() {
+    return (
+      <div className="text-editor">
+        <CustomToolbar />
+        <ReactQuill
+          value={this.state.editorHtml}
+          onChange={this.handleChange}
+          placeholder={this.props.placeholder}
+          modules={Editor.modules}
+          formats={Editor.formats}
+        />
+      </div>
+    );
+  }
+}
+
+const App = () => (
+  <div className="custom-toolbar-example">
+    <h3>Custom Toolbar with React Quill</h3>
+    <Editor placeholder={"Write something...."} />
+  </div>
+);
+
+render(<App />, document.getElementById("root"));
